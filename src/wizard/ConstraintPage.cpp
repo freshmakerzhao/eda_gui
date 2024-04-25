eb5133bc623384fc44a941bfb5593714fcf65190
0x7c06 <ksy0x7c06@outlook.com>
2024-04-25 14:25:46 +0800

    1）添加Sources显示Location,新建File显示<Local to Project>

--- src/wizard/ConstraintPage.cpp
+++ src/wizard/ConstraintPage.cpp
@@ -17,10 +17,11 @@ ConstraintPage::ConstraintPage(QWidget *parent) : QWizardPage(parent)
                 "timing constraints.");
 
     model = new QStandardItemModel(this);
-    model->setColumnCount(3); // 增加列数，用于显示索引
+    model->setColumnCount(4); // 增加列数，用于显示索引
     model->setHeaderData(0, Qt::Horizontal, QObject::tr("Index")); // 设置标题
     model->setHeaderData(1, Qt::Horizontal, QObject::tr("File Name"));
     model->setHeaderData(2, Qt::Horizontal, QObject::tr("File Type"));
+    model->setHeaderData(3, Qt::Horizontal, QObject::tr("Location"));
 
     tableView = new QTableView(this);
     tableView->setModel(model);
@@ -30,7 +31,8 @@ ConstraintPage::ConstraintPage(QWidget *parent) : QWizardPage(parent)
     tableView->verticalHeader()->hide(); //隐藏行号方法
     // tableView->horizontalHeader()->setSectionResizeMode(2,QHeaderView::Stretch);
     tableView->setColumnWidth(0, 85);
-    tableView->setColumnWidth(1, 270);
+    tableView->setColumnWidth(1, 250);
+    tableView->setColumnWidth(3, 340);
 
     QPushButton *addFilesButton = new QPushButton("Add Files");addFilesButton->setFixedSize(160, 45);
     connect(addFilesButton, &QPushButton::clicked, this, &ConstraintPage::onAddFiles);
@@ -66,6 +68,7 @@ void ConstraintPage::onAddFiles()
         }else{
             items << new QStandardItem(fileInfo.suffix());
         }
+        items << new QStandardItem(QString(fileInfo.path()));
         model->appendRow(items);
         Wizard* wizard = qobject_cast<Wizard*>(this->wizard());
         wizard->constraintFilesList.append(fileName); // 添加文件路径到列表中
@@ -94,6 +97,7 @@ void ConstraintPage::onCreateFile()
 
     QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, Qt::Horizontal, &dialog);
     formLayout->addRow(buttonBox);
+    formLayout->setVerticalSpacing(25); // 设置垂直间距
 
     connect(buttonBox, &QDialogButtonBox::accepted, [comboBox, lineEdit, &dialog, this](){
         QString fileType = comboBox->currentText();
@@ -133,6 +137,7 @@ void ConstraintPage::onCreateFile()
         }else{
             items << new QStandardItem(fileInfo.suffix());
         }
+        items << new QStandardItem("<Local to Project>");
         model->appendRow(items);
         Wizard* wizard = qobject_cast<Wizard*>(this->wizard());
         wizard->constraintFilesList.append(newFilePath); // 添加文件路径到列表中
