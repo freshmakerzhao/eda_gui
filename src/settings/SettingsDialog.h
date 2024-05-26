cc5d9aa92d070fc72b325eaa6ed318b896225488
0x7c06 <ksy0x7c06@outlook.com>
2024-05-26 20:42:09 +0800

    1）Settings:QTreeWidget+QStackedWidget

---
+++ src/settings/SettingsDialog.h
@@ -0,0 +1,29 @@
+#ifndef SETTINGSDIALOG_H
+#define SETTINGSDIALOG_H
+
+#include <QDialog>
+#include <QStackedWidget>
+#include <QTreeWidget>
+#include <QHBoxLayout>
+#include <QDialogButtonBox>
+#include <QPushButton>
+#include "GeneralPage.h"
+
+class SettingsDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit SettingsDialog(QWidget *parent = nullptr);
+
+    int exec() override;
+
+signals:
+public slots:
+
+private:
+    QStackedWidget *stackedWidget;
+    QTreeWidget *treeWidget;
+    GeneralPage *generalPage;
+};
+
+#endif // SETTINGSDIALOG_H
