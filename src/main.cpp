dc516cb0d220b1bd9e55a17b8da9b855f8067abc
ZhaoShuai <dsupermaker@163.com>
2024-07-04 11:20:25 +0800

    1）完善AES代码

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,26 @@
 #include "widgets/Form.h"
 #include "base/InitialConfig.h"
 #include "utils/LicenseUtilities.h"
-#include "base/Globals.h"
 #include "utils/AESUtilities.h"
 #include <QApplication>
 #include <QFontDatabase>
 #include <QSplashScreen>
 #include <QDateTime>
-#include <QSslSocket>
-#include <openssl/aes.h>
-#include <openssl/rand.h>
-#include <fstream>
-#include <iomanip>
 
 const int VALID_DAYS = 7;
-static unsigned char key[AES_BLOCK_SIZE*2] = "1wradfr4e3fefefad4545454h6thrsf";   //秘钥
-
-void printHex(const unsigned char* data, int length) {
-    QString hexString;
-    for (int i = 0; i < length; ++i) {
-        hexString.append(QString("%1 ").arg(data[i], 2, 16, QChar('0')).toUpper());
-    }
-    qDebug() << hexString;
-}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // 16 字节的 AES 密钥。
-    const unsigned char key_hex[16] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-    // 16 字节的初始向量（IV）
-    unsigned char iv_hex[17] = {'s','i','n','g','u','l','a','r','-','m','e','d','i','c','a','l'};
-    const char* plain_text = "zh";
-    int plain_text_len = strlen(plain_text);
-    // 计算填充长度
-    int padding_len = AES_BLOCK_SIZE - (plain_text_len % AES_BLOCK_SIZE);
-    int total_len = plain_text_len + padding_len;
+    const std::string plain_text = "z22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222hz22222222222222222222222h";
 
-    unsigned char padded_plain_text[256];
-    strcpy((char *)padded_plain_text, plain_text);
-    memset(padded_plain_text + plain_text_len, padding_len, padding_len);
+    AESUtilities::aesEncrypt(plain_text, "E:/workspace/eda_gui/src/zhaoshuai.bin");
 
-    unsigned char* encrypt_text = (unsigned char*)malloc(total_len);
-    AESUtilities::aesEncrypt((unsigned char*)plain_text, plain_text_len, key_hex, iv_hex, encrypt_text,"E:/workspace/eda_gui/src/zhaoshuai.bin");
-
-    unsigned char* decrypt_text = AESUtilities::aesDecrypt(key_hex, iv_hex,"E:/workspace/eda_gui/src/zhaoshuai.bin");
+    unsigned char* decrypt_text = AESUtilities::aesDecrypt("E:/workspace/eda_gui/src/zhaoshuai.bin");
 
     // 显示结果
-    qDebug() << "原始字符串: " << plain_text;
-    qDebug() << "加密后的密文: ";
-    printHex(encrypt_text, total_len);
+    qDebug() << "原始字符串: " << QString::fromStdString(plain_text);
     qDebug() << "解密后的字符串:";
     qDebug() << (char*)decrypt_text;
 
