7e8afa5fcbe67c76cf377d818c76b0abcbd6961f
huangzhiwei <huangzhiwei26@foxmail.com>
2024-12-12 11:04:59 +0800

    add PortBOptions widget

--- src/ipmanager/blk_mem_gen/PortBOptionsWidget.h
+++ src/ipmanager/blk_mem_gen/PortBOptionsWidget.h
@@ -2,12 +2,22 @@
 #define PORTBOPTIONSWIDGET_H
 
 #include "ipmanager/common/BasePage.h"
+#include "PortAOptionsWidget.h"
+#include <vector>
 
 class PortBOptionsWidget : public BasePage
 {
     Q_OBJECT
 public:
-    PortBOptionsWidget(QWidget *parent = nullptr);
+    PortBOptionsWidget(QWidget *parent = nullptr, PortAOptionsWidget *portA = nullptr);
+    void UpdatePortBWidth(PortAOptionsWidget *portA = nullptr);
+
+    QComboBox *portBWidthComboBox;
+    QLabel *portBDepthLabel;
+
+private:
+    void UpdatePortDepth(const QString &width);
+    int capacity;
 };
 
 #endif // PORTBOPTIONSWIDGET_H
