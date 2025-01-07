9af12fce12ee0e5ca63df6b2b87fd1f830159846
huangzhiwei <huangzhiwei26@foxmail.com>
2025-01-07 14:53:04 +0800

    update

--- src/ipmanager/blk_mem_gen/PortAOptionsWidget.cpp
+++ src/ipmanager/blk_mem_gen/PortAOptionsWidget.cpp
@@ -34,7 +34,7 @@ PortAOptionsWidget::PortAOptionsWidget(QWidget *parent) :
     portADepthLayout->addWidget(portADepthRangeLabel);
     fLayout->addRow("Port A Depth", portADepthLayout);
     fLayout->addRow(new QLabel("The Width and Depth values are used for Read Operation in Port A", this));
-    updatePortADepthRange();
+    updatePortADepthRange();//update depth range
 
     connect(portAWidthLineEdit, &QLineEdit::textChanged, this, &PortAOptionsWidget::updatePortADepthRange);
 
