edd51e86800fd2adc8dad6bd66853668b3a45e5a
ZhaoShuai <53854992+freshmakerzhao@users.noreply.github.com>
2025-05-07 00:02:28 +0800

    Dev 1 4 1 alpha (#40)
    * 更新版本号
    
    * 完善版本信息

--- src/dialog/AboutDialog.h
+++ src/dialog/AboutDialog.h
@@ -21,6 +21,8 @@ class AboutDialog : public QDialog
     Q_OBJECT
 public:
     AboutDialog(QWidget *parent = nullptr);
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
 
 };
 
